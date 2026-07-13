4c57b39dee641f58bcf38b25a0b3a415e41ae618
VED1996KATIYAR <ved750katiyar@gmail.com>
2026-07-13 08:41:40 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (87.35%) - LeetSync

---
+++ 1212-sequential-digits/sequential-digits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Integer> sequentialDigits(int low, int high) {
+        ArrayList<Integer> arr=new ArrayList<>();
+        for(int i=1;i<9;i++){
+            int sum=i;
+            for(int j=i+1;j<=9;j++){
+                sum=(sum*10)+j;
+                if(sum>=low && sum<=high){
+                    arr.add(sum);
+                }
+            }
+        }
+        Collections.sort(arr);
+        return arr;
+    }
+}
