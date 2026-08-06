d4cf20e07bfd7836dbf7a588741970b4586d44e7
VED1996KATIYAR <ved750katiyar@gmail.com>
2026-08-06 11:51:00 +0530

    Time: 9 ms (19.98%) | Memory: 46.2 MB (93.92%) - LeetSync

---
+++ 692-top-k-frequent-words/top-k-frequent-words.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class Solution {
+    // public static void main(String[] args) {
+    //     Scanner scan = new Scanner(System.in);
+    //     int n = scan.nextInt();
+    //     int words[]=new int[n];
+    //     for(int i=0;i<n;i++){
+    //         words[i]=scan.nextInt();}
+    //     int k=scan.nextInt();
+    //     List<String> arr=new ArrayList<>(topKFrequent(words,k));
+    //     System.out.println(arr);
+    // }
+    public static List<String> topKFrequent(String[] words, int k) {
+        HashMap<String,Integer> map=new HashMap<>();
+        for(String word:words){
+            if(map.containsKey(word)){
+                map.put(word,map.get(word)+1);
+                
+            }else{
+                map.put(word,1);
+            }
+        }
+        List<String> ans=new ArrayList<>(map.keySet());
+        ans.sort((a, b) -> {
+    if (map.get(a).equals(map.get(b))) {
+        return a.compareTo(b);   
+    }
+    return map.get(b) - map.get(a); 
+});
+        List<String> ans1=new ArrayList<>();
+        for(int i=0;i<k;i++){
+            ans1.add(ans.get(i));
+        }
+        return ans1;
+    }
+}
