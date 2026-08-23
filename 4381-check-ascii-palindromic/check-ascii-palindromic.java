9f231b541e7442c5ac03620288ca283a966d2338
VED1996KATIYAR <ved750katiyar@gmail.com>
2026-08-23 09:54:47 +0530

    Time: 4 ms (74.65%) | Memory: 45.3 MB (63.88%) - LeetSync

---
+++ 4381-check-ascii-palindromic/check-ascii-palindromic.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s=scan.nextLine();
+        boolean and=isPalindromic(s);
+        System.out.println(and);
+    }
+    public static boolean isPalindromic(String s) {
+        StringBuilder ss=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            int ans=ch;
+            String binary = Integer.toBinaryString(ans);
+            // जब तक लंबाई 8 न हो, आगे '0' जोड़ते रहें
+            while (binary.length() < 8) {
+                binary = "0" + binary;
+            }
+            ss.append(binary);
+        }
+        String one=ss.toString();
+        
+        
+        String two=ss.reverse().toString();
+        return one.equals(two);
+    }
+}
