790796c9a8b3bd7de1ea98d028d9297be901c6d6
VED1996KATIYAR <ved750katiyar@gmail.com>
2026-09-09 08:43:49 +0530

    Time: 1 ms (99.36%) | Memory: 42.4 MB (93.59%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
@@ -1,12 +1,20 @@
 class Solution {
     public long countCommas(long n) {
-        long t = 0;
-        
-        
-        for (long t1 = 1000; t1 <= n; t1 *= 1000) {
-            t += (n - t1 + 1);
-        }
-        
-        return t;
+        long ans=0;
+        if(n >= 1000000000000000L)
+       ans += n - 999999999999999L;
+       if(n >= 1000000000000L){
+       ans += n - 999999999999L;
+       }
+       if(n >= 1000000000L)
+       ans += n - 999999999L;
+       if(n >= 1000000)
+       ans += n - 999999;
+       if(n >= 1000)
+       ans += n - 999;
+       else
+       return 0;
+
+       return ans;
     }
 }
