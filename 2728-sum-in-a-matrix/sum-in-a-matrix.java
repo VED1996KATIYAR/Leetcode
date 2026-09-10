3d339a099dd85f804be39179d5abb91ca722c695
VED1996KATIYAR <ved750katiyar@gmail.com>
2026-09-10 07:55:05 +0530

    Time: 61 ms (7.87%) | Memory: 85.4 MB (5.43%) - LeetSync

---
+++ 2728-sum-in-a-matrix/sum-in-a-matrix.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int matrixSum(int[][] nums) {
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++){
+            ArrayList<Integer> temp = new ArrayList<>();
+            for(int j = 0; j < nums[0].length; j++){
+                temp.add(nums[i][j]);
+            }
+            temp.sort(Comparator.reverseOrder());
+            list.add(temp);
+        }
+        int sum = 0;
+        for(int j=0;j<list.get(0).size();j++){
+            int k1=Integer.MIN_VALUE;
+            for(int i=0;i<list.size();i++){
+                k1=Math.max(k1,list.get(i).get(j));
+            }
+            sum+=k1;
+        }
+        return sum;
+    }
+}
